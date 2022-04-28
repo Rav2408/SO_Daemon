358730a57a00fd002b24d060a41b81cfb67b2f19
Rav2408 <63018632+Rav2408@users.noreply.github.com>
2022-04-28 21:55:24 +0200

    Komentarze + działa po testach

--- main.c
+++ main.c
@@ -1,43 +1,35 @@
-#include <stdio.h> 			
-#include <string.h>			
-#include <stdlib.h>			
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <syslog.h>			// Możliwość użycia logu systemowego.
 #include <dirent.h>			// Udostępnia funkcje i struktury umożliwiające listowanie plików.
 #include <signal.h>			// Obsługa sygnałów.
-#include <unistd.h>			// Standardowa biblioteka dla systemów Linux udostepniająca makra i funkcje 
+#include <unistd.h>			// Standardowa biblioteka dla systemów Linux udostepniająca makra i funkcje
 #include <sys/types.h>		// Różne typy danych.
 #include <fcntl.h>			// Działanie na plikach - otwieranie, zamykanie, ustawianie uprawnień.
 #include <sys/mman.h>		// Deklaracje zarządzania pamięcią, używane do mapowania.
 #include <sys/stat.h>		// Definiuje strukury danych zwracane przez funkcje stat(), fstat(), lstat().
-#include <utime.h> 			// Modyfikacja czasu		//sprawdzic czy potrzebne
-#include <limits.h> 		// Limity systemowe (w celu pobrania PATH_MAX, czyli maksymalna dlugosc sciezki w systemie)    //sprawdzic czy potrzebne
+
 
 #define MAX_PATH_LENGTH 2048
 
-int cp_buffer = 2048;
-int recursion_option = 0;
-int sleepTime = 15;
-
-//int checkIfPathIsDirectory(char* path)
-//{
-//    struct stat statistics;
-//
-//    stat(path, &statistics);
-//
-//    if (S_ISDIR(statistics.st_mode) != 0) return 1;
-//    else return -1;
-//}
+int cp_buffer = 2048;       //domyślna wielkość buffera do kopiowania plików
+int recursion_option = 0;   //domyślnie wyłączona opcja synchronizowania podkatalogów
+int sleepTime = 300;        //5 minut - domyślny czas spania deamona
 
 
-int fileOrDir(char* path){ 							//funkcja sprawdzająca czy ścieżka jest katalogiem, czy plikiem
+//funkcja sprawdzająca czy ścieżka jest katalogiem = 0, czy plikiem = 1
+int fileOrDir(char* path){
     struct stat fileStatistic;
-    stat(path,&fileStatistic);x`
+    stat(path,&fileStatistic);
     if(S_ISREG(fileStatistic.st_mode)) return 1;	//regular file
     if(S_ISDIR(fileStatistic.st_mode)) return 0;    //directory
     return -1;      //error
 }
 
-void signalHandle(int sig){ 												// Obsługa sygnału - funkcja wypisująca do logu zależnie od typu sygnału
+
+// Obsługa sygnału - funkcja wypisująca do logu zależnie od typu sygnału
+void signalHandle(int sig){
     if(sig == SIGALRM) syslog(LOG_INFO, "Daemon woken up automatically");
     else if (sig == SIGUSR1) syslog(LOG_INFO, "Daemon woken up by SIGUSR1 signal");
 }
@@ -46,7 +38,7 @@ void setSignal(struct sigaction newSignal, sigset_t newSet, int sig){  //
     sigemptyset(&newSet);
 
     newSignal.sa_handler = &signalHandle;
-    newSignal.sa_flags = 0;						
+    newSignal.sa_flags = 0;
     newSignal.sa_mask = newSet;
 
     sigaction(sig, &newSignal, NULL);
@@ -70,7 +62,9 @@ int checkParameters(int argc, char* argv[]){ 		// Funkcja sprawdzająca poprawno
     }
 }
 
-void setParameters(int argc, char* argv[]){			  // Funkcja sprawdzające parametry podane przez użytkownika do tablicy argv
+
+// Funkcja sprawdzające parametry podane przez użytkownika do tablicy argv
+void setParameters(int argc, char* argv[]){
     for (int i = 3; i < argc; i++){
         if (argv[i][0] == '-' && argv[i][1] == 't'){  //poszukiwanie flagi -t zmieniającej czas czuwania demona
             if (i+1 < argc && atoi(argv[i+1]) > 0){
@@ -88,7 +82,8 @@ void setParameters(int argc, char* argv[]){			  // Funkcja sprawdzające paramet
     }
 }
 
-char* pathLinking(char* path, char* fName){					//Funkcja, która tworzy ścieżki do rekurencyjnej synchronizacji
+//Funkcja, która tworzy ścieżki do rekurencyjnej synchronizacji
+char* pathLinking(char* path, char* fName){
     char* fullPath = malloc(MAX_PATH_LENGTH*sizeof(char));
     strcpy(fullPath,path);
     strcat(fullPath,"/");
@@ -96,7 +91,9 @@ char* pathLinking(char* path, char* fName){					//Funkcja, która tworzy ścież
     return (char*)fullPath;
 }
 
-int readWriteCopy(char* src, char* dst){ 				
+
+//Funkcja, która kopiuje pliki używając do tego buffora
+int readWriteCopy(char* src, char* dst){
     FILE* srcFile = fopen(src, "rb");
     FILE* dstFile = fopen(dst, "wb");
 
@@ -114,7 +111,7 @@ int readWriteCopy(char* src, char* dst){
     size_t in,out;
 
     syslog(LOG_ERR, "Copying started...");
-    while (1){
+    while (1){      //jeżeli skończły się elementy do kopiowania zmienna in będzie równa 0
         in = fread(buffer, 1, cp_buffer, srcFile);
         if( in == 0 ) break;
         out = fwrite(buffer, 1, in, dstFile);
@@ -127,6 +124,8 @@ int readWriteCopy(char* src, char* dst){
     return 0;
 }
 
+
+//Funkcja, która kopiuje pliki używając do tego mapowania
 int mapCopy(char* src, char* dst, struct stat* srcStat){
     int srcFile = open(src, O_RDONLY);
     int dstFile = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0777);
@@ -141,9 +140,10 @@ int mapCopy(char* src, char* dst, struct stat* srcStat){
         return 1;
     }
 
-    char* buffer = mmap(0, srcStat->st_size, PROT_READ, MAP_SHARED, srcFile, 0);    //sprawdzić czy nie ma problemu z bufferem
+    char* buffer = mmap(0, srcStat->st_size, PROT_READ, MAP_SHARED, srcFile, 0);
+
     syslog(LOG_ERR, "Copying started...");
-    if(write(dstFile, buffer, srcStat->st_size) < 0)
+    if(write(dstFile, buffer, srcStat->st_size) < 0 )   //Jeżeli wystąpi błąd
     {
         syslog(LOG_ERR, "Error on writing the file");
         return 1;
@@ -152,12 +152,18 @@ int mapCopy(char* src, char* dst, struct stat* srcStat){
     return 0;
 }
 
-int checkAndSync(char* src,char* dst){		// Funkcja synchronizująca katalogi
+
+// Funkcja synchronizująca katalogi
+int checkAndSync(char* src,char* dst){
     DIR* srcDir = opendir(src);
     DIR* dstDir = opendir(dst);
 
-    struct dirent *currentFile;				// Biblioteka dirent.h udostępnia strukturę dirent, która określa aktualny element katalogu
+
+    // Biblioteka dirent.h udostępnia strukturę dirent, która określa aktualny element katalogu
+    struct dirent *currentFile;
+    // Biblioteka <sys/stat.h> udostępnia strukturę stat, przechowującą niektóre statystyki pliku
     struct stat srcFStat, dstFStat;
+    //zmienna określająca czy plik istnieje, a jeśli tak to czy jest plikiem =1 lub katalogiem =0
     int file_exist;
 
     char* srcFilePath;
@@ -178,14 +184,14 @@ int checkAndSync(char* src,char* dst){		// Funkcja synchronizująca katalogi
             srcFilePath = pathLinking(src, currentFile->d_name);
             dstFilePath = pathLinking(dst, currentFile->d_name);
 
-            dstFStat.st_mtime = 0; //jeśli plik nie istniał to czas modyfikacji jest równy zero
+            dstFStat.st_mtime = 0; //Jeśli plik nie istniał to czas modyfikacji jest równy zero
             stat(srcFilePath,&srcFStat);
-            file_exist = stat(dstFilePath,&dstFStat);   //jeśli plik istnieje to czas modyfikacji jest nadpisywany
+            file_exist = stat(dstFilePath,&dstFStat);   //Jeśli plik istnieje to czas modyfikacji jest nadpisywany
 
             if(fileOrDir(srcFilePath) && srcFStat.st_mtime > dstFStat.st_mtime){  // Jeśli czas modyfikacji pliku źródłowego jest nowszy, należy kopiować
                 if(srcFStat.st_size <= cp_buffer) readWriteCopy(srcFilePath,dstFilePath);
                 else mapCopy(srcFilePath, dstFilePath, &srcFStat);
-				chmod(dstFilePath, srcFStat.st_mode); 
+                chmod(dstFilePath, srcFStat.st_mode);
             }else if(fileOrDir(srcFilePath) == 0 && recursion_option){ //  Jeżeli ścieżka jest katalogiem i użytkownik wybrał opcję rekurencyjnego kopiowania plików
                 if(file_exist == -1){
                     mkdir(dstFilePath,srcFStat.st_mode);
@@ -202,12 +208,15 @@ int checkAndSync(char* src,char* dst){		// Funkcja synchronizująca katalogi
     return 0;
 }
 
-int checkAndDelete(char* src,char* dst) { 		// Funkcja usuwająca pliki które nie znajdują się w ścieżce źródłowej
+
+// Funkcja usuwająca pliki z katalogu docelowego, które nie znajdują się w w katalogu źródłowym
+int checkAndDelete(char* src,char* dst) {
     DIR *srcDir = opendir(src);
     DIR *dstDir = opendir(dst);
 
     struct dirent *currentFile;
     struct stat srcFStat, dstFStat;
+    //zmienna określająca czy plik istnieje, a jeśli tak to czy jest plikiem =1 lub katalogiem =0
     int file_exist;
 
     char *srcFilePath;
@@ -219,25 +228,25 @@ int checkAndDelete(char* src,char* dst) { 		// Funkcja usuwająca pliki które n
     }
 
     while ((currentFile = readdir(dstDir)) != NULL){
-        if((strcmp(currentFile->d_name, ".") != 0) && (strcmp(currentFile->d_name, "..") != 0) ){ 
+        if((strcmp(currentFile->d_name, ".") != 0) && (strcmp(currentFile->d_name, "..") != 0) ){
             srcFilePath = pathLinking(src, currentFile->d_name);
             dstFilePath = pathLinking(dst, currentFile->d_name);
 
             lstat(dstFilePath,&dstFStat);
             file_exist = fileOrDir(dstFilePath);
 
-            if(file_exist == 1){    //jest zywkłym plikiem
+            if(file_exist == 1){    //plik w katalogu docelowym jest zywkłym plikiem
                 if(lstat(srcFilePath,&srcFStat) == -1){  //w katalogu źródłowym nie istnieje taki plik
                     unlink(dstFilePath);
                     syslog(LOG_INFO, "File has been deleted1: %s", dstFilePath);
-                }else if(file_exist == 0){      //jest katalogiem
+                }else if(fileOrDir(srcFilePath) == 0){      //plik w katalogu źrodłowym jest katalogiem
                     unlink(dstFilePath);
                     syslog(LOG_INFO, "File has been deleted2: %s", dstFilePath);
                 }
-            }else if(file_exist == 0 && recursion_option){      //jest katalogiem
+            }else if(file_exist == 0 && recursion_option){      //plik w katalogu docelowym jest katalogiem i jest opcja -R
                 if(lstat(srcFilePath,&srcFStat) == -1){       //w źródłowym katalogu nie ma takiego katalogu
-                    checkAndDelete(srcFilePath,dstFilePath);
-                    rmdir(dstFilePath);
+                    checkAndDelete(srcFilePath,dstFilePath);    //usuń najpierw pliki w podkatalogu
+                    rmdir(dstFilePath);                                 //usuń katalog
                     syslog(LOG_INFO, "Directory has been deleted: %s", dstFilePath);
                 }else{      //w źródłowym katalogu jest taki katalog
                     checkAndDelete(srcFilePath,dstFilePath);
@@ -256,7 +265,8 @@ int checkAndDelete(char* src,char* dst) { 		// Funkcja usuwająca pliki które n
 int main(int argc, char* argv[]){
     struct sigaction autoSig, userSig;
     sigset_t autoSet, userSet;
-    // Open a log file
+
+    //Otwieranie logu, można przeczytać go za pomocą polecenia: tail -f /var/log/syslog
     openlog("SynchronizeDemon", LOG_PID, LOG_LOCAL0);
     if(checkParameters(argc,argv) == -1) exit(-1);
     setParameters(argc,argv);
@@ -264,28 +274,27 @@ int main(int argc, char* argv[]){
     pid_t process_id = 0;
     pid_t sid = 0;
 
-    // Create child process
+    //Stworzenie procesu dziecka
     process_id = fork();
 
-    // Indication of fork() failure
+    //Zakończenie procesu w przypadku błędu
     if (process_id < 0){
         printf("fork failed!\n");
-        exit(1);    // Return failure in exit status
+        exit(1);
     }
 
-    // PARENT PROCESS. Need to kill it.
+    //Zakończenie procesu w przypadku gdy jest to proces-rodzic
     if (process_id > 0){
         printf("process_id of child process %d \n", process_id);
-        exit(0);    // return success in exit status
+        exit(0);
     }
 
-    //unmask the file mode?
     umask(0);
 
-    //set new session
+    //ustawienie nowej sesji
     sid = setsid();
     if(sid < 0){
-        exit(1);    // Return failure
+        exit(1);
     }
 
 
@@ -296,7 +305,7 @@ int main(int argc, char* argv[]){
         setSignal(userSig, userSet, SIGUSR1);
         syslog(LOG_INFO, "SynchronizeDemon fell asleep");
         alarm(sleepTime);
-        pause();    //bez pauzy powtarza komunikaty w syslogu
+        pause();
         checkAndSync(argv[1], argv[2]); //sprawdzanie katalogu źródłowego w celu kopiowania
         checkAndDelete(argv[1], argv[2]); //sprawdzanie katalogu docelowego w celu usuwania
 
